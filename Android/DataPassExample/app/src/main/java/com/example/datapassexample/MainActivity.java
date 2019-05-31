5db452b50dab7f18816642f94c3b00154de99f23
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-05-31 15:49:21 +0600

    Data_Pass_Example_Update

--- Android/DataPassExample/app/src/main/java/com/example/datapassexample/MainActivity.java
+++ Android/DataPassExample/app/src/main/java/com/example/datapassexample/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.datapassexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button secondActivityBtn;
+    EditText dataEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        secondActivityBtn = findViewById(R.id.secondActivityBtn);
+        dataEt = findViewById(R.id.dataET);
+
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String value = dataEt.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("batch5", value);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Android/DataPassExample/app/src/main/java/com/example/datapassexample/SecondActivity.java
+++ Android/DataPassExample/app/src/main/java/com/example/datapassexample/SecondActivity.java
@@ -2,12 +2,21 @@ package com.example.datapassexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        textView = findViewById(R.id.textView);
+
+        String data = getIntent().getStringExtra("batch5");
+
+        textView.setText(data);
     }
 }
