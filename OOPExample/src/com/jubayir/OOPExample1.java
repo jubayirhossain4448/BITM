68eed6933883569d724eabe6119431dd20eeec41
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-12 13:12:18 +0600

    OOP_Example_Update

--- OOPExample/src/com/jubayir/OOPExample1.java
+++ OOPExample/src/com/jubayir/OOPExample1.java
@@ -9,9 +9,21 @@ public class OOPExample1 {
         person1.firstName = "Md.";
         person1.middleName = "Jubayir";
         person1.lastName = "Hossain";
+//        person1.age = 27;
+//        person1.roll = 243751;
+//        person1.mobile = 8282;
 
         String fullName = person1.getFullName();
         System.out.println(fullName);
+
+//        String number = person1.getNumbers();
+//        System.out.println(number);
+        Person person2 = person1;
+        person2.lastName = "Hasan";
+        fullName = person1.lastName;
+        System.out.println(fullName);
+
+        person2 = person1;
     }
 
 }
