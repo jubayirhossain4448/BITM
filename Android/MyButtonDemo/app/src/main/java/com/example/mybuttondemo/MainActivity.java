dd6ad46b427f3fb6ec70a45d3a5f2f976b5306b9
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-14 23:26:59 +0600

    My_Button_Demo_update2

--- Android/MyButtonDemo/app/src/main/java/com/example/mybuttondemo/MainActivity.java
+++ Android/MyButtonDemo/app/src/main/java/com/example/mybuttondemo/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.mybuttondemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        textView = findViewById(R.id.txtV);
+
+        
     }
 }
