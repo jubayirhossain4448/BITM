1eb8380051c712fad51597f089f0690e1729aa20
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-14 23:32:35 +0600

    My_Button_Demo_update3

--- Android/MyButtonDemo/app/src/main/java/com/example/mybuttondemo/MainActivity.java
+++ Android/MyButtonDemo/app/src/main/java/com/example/mybuttondemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mybuttondemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         loginBtn = findViewById(R.id.loginBtn);
         textView = findViewById(R.id.txtV);
 
-        
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Login button is clicked");
+            }
+        });
     }
 }
