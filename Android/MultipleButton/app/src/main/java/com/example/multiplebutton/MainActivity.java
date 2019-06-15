ae770cd8379288d377de268e96415a5b4a4c3cf4
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-16 00:33:58 +0600

    Multiple_Button_update1

--- Android/MultipleButton/app/src/main/java/com/example/multiplebutton/MainActivity.java
+++ Android/MultipleButton/app/src/main/java/com/example/multiplebutton/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.multiplebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        textView = findViewById(R.id.txtV);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textView.setText("Login button is Clicked! ");
+            }
+        });
     }
 }
