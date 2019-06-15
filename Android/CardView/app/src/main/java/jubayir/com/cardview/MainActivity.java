d4019d86b2b38a49bd6bdf95ba9a6cfdffd35afc
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-15 12:04:51 +0600

    Card_View_Example_Option_Menu

--- Android/CardView/app/src/main/java/jubayir/com/cardview/MainActivity.java
+++ Android/CardView/app/src/main/java/jubayir/com/cardview/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -28,5 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public boolean onCreateOnptionMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options, menu); // options here for xml resourse name
+        return true;
     }
 }
