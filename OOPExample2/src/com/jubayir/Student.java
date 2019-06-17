5dce14554c0693f912e267b937943de8f8b2798d
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-17 11:01:01 +0600

    OOPExample2

---
+++ OOPExample2/src/com/jubayir/Student.java
@@ -0,0 +1,15 @@
+package com.jubayir;
+
+public class Student {
+    String id;
+    String name;
+    String roll;
+    String fathersName;
+    String mothersName;
+
+    public Student() {
+    }
+    
+    
+    
+}
---
+++ OOPExample2/src/com/jubayir/Test.java
@@ -0,0 +1,9 @@
+package com.jubayir;
+
+public class Test {
+
+    public static void main(String[] args) {
+        new Student();
+    }
+
+}
