79e78e9aef18d4fe50ec5dd2d6a371aae6063f62
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-17 12:38:27 +0600

    OOPExample_Update

--- OOPExample2/src/com/jubayir/Student.java
+++ OOPExample2/src/com/jubayir/Student.java
@@ -9,6 +9,41 @@ public class Student {
 
     public Student() {
     }
+
+    public Student(String id, String name, String roll, String fathersName, String mothersName) {
+        this.id = id;
+        this.name = name;
+        this.roll = roll;
+        this.fathersName = fathersName;
+        this.mothersName = mothersName;
+    }
+    
+    Student(String id, String name, String roll, String fathersName){
+    this.id = id;
+    this.name = name;
+    this.roll = roll;
+    this.fathersName = fathersName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public String getFathersName() {
+        return fathersName;
+    }
+
+    public String getMothersName() {
+        return mothersName;
+    }
     
     
     
--- OOPExample2/src/com/jubayir/Test.java
+++ OOPExample2/src/com/jubayir/Test.java
@@ -3,7 +3,10 @@ package com.jubayir;
 public class Test {
 
     public static void main(String[] args) {
-        new Student();
+        Student student = new Student("J-243751", "Md. Jubayir Hossain", "1244149", "Abdus Shukur", "Asia Khatun");
+        System.out.println(student.getId());
+        System.out.println(student.getId());
+        System.out.println(student.getId());
     }
 
 }
