59684c432d4943ccbb5052071bf39293e802f8fa
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 11:42:16 +0600

    Constructor_Adding

--- Android_Home/OOPExample/src/com/jubayir/Person.java
+++ Android_Home/OOPExample/src/com/jubayir/Person.java
@@ -1,9 +1,22 @@
 package com.jubayir;
+
 public class Person {                // Pascale case(First letter of every word would be capital letter)
+
     String id;
     String name;
     String email;
     String gender;
     String mobileNumber;            // Camel case (First letter of first word would be small letter and then every word's first letter would be capital letter)
-    
+
+    public Person() {
+    }
+
+    public Person(String id, String name, String email, String gender, String mobileNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.mobileNumber = mobileNumber;
+    }
+
 }
---
+++ Android_Home/OOPExample/src/com/jubayir/Test.java
@@ -0,0 +1,9 @@
+package com.jubayir;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person);
+    }
+    
+}
