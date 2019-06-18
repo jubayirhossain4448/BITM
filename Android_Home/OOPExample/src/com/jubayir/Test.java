25879855d22750fbd54e3b7a979c61d072be3483
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 12:07:34 +0600

    Print_result By Adding getter_method

--- Android_Home/OOPExample/src/com/jubayir/Test.java
+++ Android_Home/OOPExample/src/com/jubayir/Test.java
@@ -3,13 +3,14 @@ package com.jubayir;
 public class Test {
 
     public static void main(String[] args) {
-        Person person = new Person();           // Object create korar time e or Object create korte hole Constructor create korte hoi
-        person.id = "Round-37";
-        person.name = "Md. Jubayir Hossain";
-        person.email = "jubayirhossain4448@gmail.com";
-        person.gender = "Male";
-        person.mobileNumber = "01918718282";
-        System.out.println("The result of Object is : " + " " + person.id + " " + person.name + " " + person.email + " " + person.gender + " " + person.mobileNumber);
+        Person person = new Person("J-1244149", "Md. Jubayir Hossain", "jubayir4448@gmail.com", "Male", "01918718282");           // Object create korar time e or Object create korte hole Constructor create korte hoi
+//        person.id = "Round-37";
+//        person.name = "Md. Jubayir Hossain";
+//        person.email = "jubayirhossain4448@gmail.com";
+//        person.gender = "Male";
+//        person.mobileNumber = "01918718282";
+//        System.out.println("The result of Object is : " + " " + person.id + " " + person.name + " " + person.email + " " + person.gender + " " + person.mobileNumber);
+        System.out.println("Details of a Person " + " " + person.id + " " + person.name + " " + person.email + " " + person.gender + " " + person.mobileNumber);
     }
 
 }
