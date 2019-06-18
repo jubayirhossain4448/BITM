5b4de3137522940b50dab5408ffc6ea82a38b42c
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-18 11:15:51 +0600

    Class_Method_Constructor_Access_Modifier_Data_Hiding_Constructor_chaining

---
+++ Android_Home/OOPExample/src/com/jubayir/Person.java
@@ -0,0 +1,9 @@
+package com.jubayir;
+public class Person {                // Pascale case(First letter of every word would be capital letter)
+    String id;
+    String name;
+    String email;
+    String gender;
+    String mobileNumber;            // Camel case (First letter of first word would be small letter and then every word's first letter would be capital letter)
+    
+}
