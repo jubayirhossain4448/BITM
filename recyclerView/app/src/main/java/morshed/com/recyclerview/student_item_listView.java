c6f430648aea119b87b7082ddaa395a043cbe714
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-19 12:52:11 +0600

    Layout_adding

---
+++ recyclerView/app/src/main/java/morshed/com/recyclerview/student_item_listView.java
@@ -0,0 +1,13 @@
+package morshed.com.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class student_item_listView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_item_list_view);
+    }
+}
