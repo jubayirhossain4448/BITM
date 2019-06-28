6b8acae86e5b2ea3b802a42fba80852e6f5eb7d9
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-28 19:34:57 +0600

    My_Bottom_navigation_Adding_CustomerAdapter_update

--- Android_Home/MyBottomNavigation/app/src/main/java/com/jubayir/CustomerAdapter.java
+++ Android_Home/MyBottomNavigation/app/src/main/java/com/jubayir/CustomerAdapter.java
@@ -2,29 +2,55 @@ package com.jubayir;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.ViewHolder> {
+
+    private List<User> users;
+
+    public CustomerAdapter(List<User> users) {
+        this.users = users;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclearview_user_list, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
 
+        User user = users.get(i);
+
+        viewHolder.name.setText(user.getName());
+        viewHolder.id.setText(user.getId());
+        viewHolder.phoneNo.setText(user.getPhoneNo());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return users.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name, id, phoneNo;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            name = itemView.findViewById(R.id.nameTV);
+            id = itemView.findViewById(R.id.idTV);
+            phoneNo = itemView.findViewById(R.id.phoneTV);
         }
     }
 }
