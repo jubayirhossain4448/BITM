578f4877e6f48f113471020047d79adf02af45cd
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-01 12:00:49 +0600

    Sqlite_Java_Update

--- SqliteDemo/app/src/main/java/tania/com/sqlitedemo/DataBaseHelper.java
+++ SqliteDemo/app/src/main/java/tania/com/sqlitedemo/DataBaseHelper.java
@@ -1,5 +1,6 @@
 package tania.com.sqlitedemo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,7 +11,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, VERSION);
     }
 
-    private static String DATABASE_NAME = "User.db";
+    private static String DATABASE_NAME = "User.db";  // database er name create korte hobe
     private static String TABLE_NAME = "User";
     private static String COL_ID = "Id";
     private static String COL_NAME = "Name";
@@ -28,5 +29,16 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public long insertData(String name, String age){
+
+        ContentValues values = new ContentValues();
+        values.put(COL_NAME, name);
+        values.put(COL_AGE, age);
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        long id = sqLiteDatabase.insert(TABLE_NAME, null, values);
+        sqLiteDatabase.close();
+        return id;
     }
 }
--- SqliteDemo/app/src/main/java/tania/com/sqlitedemo/MainActivity.java
+++ SqliteDemo/app/src/main/java/tania/com/sqlitedemo/MainActivity.java
@@ -3,12 +3,15 @@ package tania.com.sqlitedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private EditText nameEt, age;
     private Button insertBTN;
+    private String aName, aAge;
+    private DataBaseHelper dataBaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+
+        insertBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aName = nameEt.getText().toString();
+                aAge = age.getText().toString();
+                dataBaseHelper.insertData(aName, aAge);
+            }
+        });
     }
 
     private void init() {
         nameEt = findViewById(R.id.nameET);
         age = findViewById(R.id.ageET);
         insertBTN = findViewById(R.id.insertBTN);
+        dataBaseHelper = new DataBaseHelper(this);
 
     }
 
