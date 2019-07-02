bef9d7423c3c3dec6c9d28d47616b90419c4184e
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-03 00:12:21 +0600

    Bottom_Sheet_Java_Class

---
+++ Android_Home/BottomSheet/app/src/main/java/com/example/bottomsheet/DailyExpenseBottomSheet.java
@@ -0,0 +1,19 @@
+package com.example.bottomsheet;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DailyExpenseBottomSheet extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.bottom_sheet_daily_expenses_details, container, false);
+        return v;
+    }
+}
