96592848545c71a9899f81b15d635792f15e00bb
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-03 00:28:22 +0600

    Bottom_Sheet_Java_Update

--- Android_Home/BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.java
+++ Android_Home/BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.bottomsheet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button open;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        open = findViewById(R.id.openBottomSheetBtn);
+
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DailyExpenseBottomSheet dailyExpenseBottomSheet = new DailyExpenseBottomSheet();
+                dailyExpenseBottomSheet.show(getSupportFragmentManager(), "Daily Expenses");
+            }
+        });
     }
 }
