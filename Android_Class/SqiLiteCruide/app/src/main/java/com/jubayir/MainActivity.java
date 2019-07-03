4da34c44c888c4a084a62cfb77e9d082ea690d36
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-03 09:45:22 +0600

    Sql_Cruide_JavaClass_Creating

--- Android_Class/SqiLiteCruide/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Class/SqiLiteCruide/app/src/main/java/com/jubayir/MainActivity.java
@@ -3,12 +3,35 @@ package com.jubayir;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameET, ageET;
+    private Button insertBTN, showDetailsBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        insertBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void init() {
+
+        nameET = findViewById(R.id.nameET);
+        ageET = findViewById(R.id.ageET);
+        insertBTN = findViewById(R.id.insertBTN);
+        showDetailsBTN =findViewById(R.id.showDetailsBTN);
     }
 }
