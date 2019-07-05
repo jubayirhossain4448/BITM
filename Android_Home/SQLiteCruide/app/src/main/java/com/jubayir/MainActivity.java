4d8163d185d941ad2a6d7d702e1fc09db9ce6dc1
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-05 22:16:53 +0600

    SQLite_Cruide_DatabaseHelper_Adding

--- Android_Home/SQLiteCruide/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Home/SQLiteCruide/app/src/main/java/com/jubayir/MainActivity.java
@@ -1,14 +1,30 @@
 package com.jubayir;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    /* 1.First activity_main.xml create korte hobe.
+    2.Main Activity te id gulo initialize korte hobe.
+    3. Button er Jonno ekta ShowDataActivity nite hobe.
+    4. showDataActivity te SQLiteOpenHelper class e extends korte hobe.
+    5. // Database and Field and Table create korte hobe.
+    6. data insert er Jonno ekta class korte hobe
+    7. insertData Method theke data nite hole main activity te ekta object create korte hobe
+    */
+
     private EditText nameET, ageET;
     private Button insertBtn, showDataBtn;
     private String name, age;
+    // insertData Method theke data nite hole main activity te ekta object create korte hobe
+    private DatabaseHelper helper;
+
+    // Two
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +32,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        // Two
+
+        insertBtn.setOnClickListener(new View.OnClickListener() { // button e click korle edit text er data nie string e rakthe hobe
+            @Override
+            public void onClick(View v) {
+                name = nameET.getText().toString();
+                age = ageET.getText().toString();
+                long id = helper.insertData(name, age); // Method calling here
+                Toast.makeText(MainActivity.this, "" +id, Toast.LENGTH_SHORT).show();
+
+// Three
+            }
+        });
 
 
-
+        showDataBtn.setOnClickListener(new View.OnClickListener() { // main activity te click korle Show data activity te data jabe
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ShowDataActivity.class));
+            }
+        });
+// Three
     }
 
-    private void init() {
+    private void init() { // initialize er jonno
+        // Two
         nameET = findViewById(R.id.nameET);
         ageET = findViewById(R.id.ageET);
         insertBtn = findViewById(R.id.insertBtn);
         showDataBtn = findViewById(R.id.showDataBtn);
+        helper = new DatabaseHelper(this); // Database helper initialize korte hobe
+        // Seven
+
     }
 }
