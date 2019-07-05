10c4871d888be8183325c3779644dd79b7fb0a3c
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-05 20:52:47 +0600

    SQLite_Cruide_initialization

--- Android_Home/SQLiteCruide/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Home/SQLiteCruide/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,12 +2,29 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText nameET, ageET;
+    private Button insertBtn, showDataBtn;
+    private String name, age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+
+
+    }
+
+    private void init() {
+        nameET = findViewById(R.id.nameET);
+        ageET = findViewById(R.id.ageET);
+        insertBtn = findViewById(R.id.insertBtn);
+        showDataBtn = findViewById(R.id.showDataBtn);
     }
 }
