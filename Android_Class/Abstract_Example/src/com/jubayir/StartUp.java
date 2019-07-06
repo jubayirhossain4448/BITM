eed29102a3b27511787a7e0591d6885b68eba500
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-06 11:54:26 +0600

    WebDev_Class

--- Android_Class/Abstract_Example/src/com/jubayir/StartUp.java
+++ Android_Class/Abstract_Example/src/com/jubayir/StartUp.java
@@ -3,8 +3,11 @@ package com.jubayir;
 public class StartUp {
     
     public static void main(String[] args) {
-        AndroidDev dev = new AndroidDev();
-        System.out.println("Salary " + dev.getSalary());
+//        AndroidDev dev = new AndroidDev();
+//        System.out.println("Salary " + dev.getSalary());
+        
+        WebDev webDev = new WebDev();
+        System.out.println("WebDev_Salary: " + webDev.salary);
     }
     
 }
--- Android_Class/Abstract_Example/src/com/jubayir/WebDev.java
+++ Android_Class/Abstract_Example/src/com/jubayir/WebDev.java
@@ -1,7 +1,12 @@
 package com.jubayir;
 
-public class WebDev {
+public class WebDev extends Developer{
     public int salary = 35000;
+
+    @Override
+    public int getSalary() {
+        return salary;
+    }
     
     
 }
