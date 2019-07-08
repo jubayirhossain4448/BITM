42c4d0d1b7b647f197f822790e78623006c65bbe
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-08 13:01:13 +0600

    Camera_Gallery_update

--- Android_Class/CameraGallery/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Class/CameraGallery/app/src/main/java/com/jubayir/MainActivity.java
@@ -1,8 +1,13 @@
 package com.jubayir;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+    public void OpenGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("Image/");
+        startActivityForResult(intent, 1);
+    }
+
+    public void OpenCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+
+            if(requestCode == 0 ){
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap =(Bitmap) bundle.get("data");
+            }
+        }
+
     }
 }
