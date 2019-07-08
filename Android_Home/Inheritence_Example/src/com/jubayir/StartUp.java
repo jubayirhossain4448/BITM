334cb166b6d92af405eec53e0d91f624698525bc
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-09 01:39:13 +0600

    Camera_Gallery

--- Android_Home/Inheritence_Example/src/com/jubayir/StartUp.java
+++ Android_Home/Inheritence_Example/src/com/jubayir/StartUp.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jubayir;
 
-/**
- *
- * @author MD.JUBAYIR HOSSAIN
- */
 public class StartUp {
+    
+    public static void main(String[] args) {
+        
+    }
     
 }
