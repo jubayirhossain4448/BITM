e487cb810cdf490800a5038b241ea29e8708e28f
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-08 11:28:28 +0600

    DatePicker

--- Android_Class/DatePicker/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Class/DatePicker/app/src/main/java/com/jubayir/MainActivity.java
@@ -1,14 +1,77 @@
 package com.jubayir;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button openDatePickerBtn, openTimePickerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openDatePickerBtn = findViewById(R.id.openDatePickerBtn);
+        openTimePickerBtn = findViewById(R.id.openTimePickerBtn);
+
+        openDatePickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openDatePicker();
+
+            }
+        });
+
+        openTimePickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void openDatePicker() {
+        final DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+            @TargetApi(Build.VERSION_CODES.N)
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month =  month + 1;
+                String pickedDate = year + "/" + month + "/" + day + "/" + "00:00:00";
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = null;
+                try {
+                    date = dateFormat.parse(pickedDate);
+                } catch (ParseException e) {
+                    openDatePickerBtn.setTag(dateFormat.format(date));
+                }
+                Toast.makeText(MainActivity.this, ""+ month, Toast.LENGTH_SHORT).show();
+
+            }
+        };
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
     }
 }
