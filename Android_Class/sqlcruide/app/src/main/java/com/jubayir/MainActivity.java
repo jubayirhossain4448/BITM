578b23c187f8b304ea4a161708de4d990682960b
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-08 09:25:31 +0600

    Update

--- Android_Class/sqlcruide/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Class/sqlcruide/app/src/main/java/com/jubayir/MainActivity.java
@@ -1,12 +1,13 @@
 package com.jubayir;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     /* 1.First activity_main.xml create korte hobe.
