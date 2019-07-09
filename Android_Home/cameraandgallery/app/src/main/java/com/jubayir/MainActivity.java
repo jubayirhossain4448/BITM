7b29d4453db6c738d1f35a11b440a56b99567db3
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-10 00:33:38 +0600

    Onclick

--- Android_Home/cameraandgallery/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Home/cameraandgallery/app/src/main/java/com/jubayir/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jubayir;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // One
+        startActivityForResult(intent, 0); // two
+    }
+
+    public void openGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); // Three
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
     }
 }
