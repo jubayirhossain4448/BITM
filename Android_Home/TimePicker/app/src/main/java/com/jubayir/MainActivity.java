0ac60e79ec1ef9a9c5a0a405a0b7b91581518dcc
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-09 22:38:54 +0600

    activity_main

--- Android_Home/TimePicker/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Home/TimePicker/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,8 +2,12 @@ package com.jubayir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
