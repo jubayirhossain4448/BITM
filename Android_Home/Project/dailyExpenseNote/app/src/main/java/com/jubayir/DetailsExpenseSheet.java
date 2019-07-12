83996a661a2c1c08a2059981c24dd7221ffe06f6
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-12 20:20:09 +0600

    activity_details_sheet

---
+++ Android_Home/Project/dailyExpenseNote/app/src/main/java/com/jubayir/DetailsExpenseSheet.java
@@ -0,0 +1,13 @@
+package com.jubayir;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsExpenseSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_expense_sheet);
+    }
+}
