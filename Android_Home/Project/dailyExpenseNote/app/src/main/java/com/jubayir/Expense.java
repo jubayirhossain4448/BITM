f31087290d2474a272a175b1f01ce6a8f5cf5964
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-12 20:45:34 +0600

    Expense_java_Class

--- Android_Home/Project/dailyExpenseNote/app/src/main/java/com/jubayir/Expense.java
+++ Android_Home/Project/dailyExpenseNote/app/src/main/java/com/jubayir/Expense.java
@@ -1,4 +1,42 @@
 package com.jubayir;
 
+import android.graphics.Bitmap;
+
 public class Expense {
+    private int amount, id;
+    private String type, date, time;
+    private Bitmap imageDocument;
+
+    public Expense(int amount, int id, String type, String date, String time, Bitmap imageDocument) {
+        this.amount = amount;
+        this.id = id;
+        this.type = type;
+        this.date = date;
+        this.time = time;
+        this.imageDocument = imageDocument;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public Bitmap getImageDocument() {
+        return imageDocument;
+    }
 }
