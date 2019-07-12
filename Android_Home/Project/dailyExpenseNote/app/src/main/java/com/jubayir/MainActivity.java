033f821e4fb55cab9655344e022850ad15794dbf
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-12 20:58:35 +0600

    Main_Activity

--- Android_Home/Project/dailyExpenseNote/app/src/main/java/com/jubayir/MainActivity.java
+++ Android_Home/Project/dailyExpenseNote/app/src/main/java/com/jubayir/MainActivity.java
@@ -2,13 +2,18 @@ package com.jubayir;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private FrameLayout frameLayout;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_Expenses:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -34,9 +39,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
+        replaceFragment(new DashboardFragment());
+
+        BottomNavigationView navView = findViewById(R.id.navigation);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        frameLayout = findViewById(R.id.frameLayoutID);
+
+    }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.replace(R.id.frameLayoutID,fragment);
+        ft.commit();
     }
 
 }
