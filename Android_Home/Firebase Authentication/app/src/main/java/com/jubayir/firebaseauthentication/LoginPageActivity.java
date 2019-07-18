747c2eff370d1920738a2e82ff4d94f01ac149b0
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-19 00:16:06 +0600

    LoginPageActivity

--- Android_Home/Firebase Authentication/app/src/main/java/com/jubayir/firebaseauthentication/LoginPageActivity.java
+++ Android_Home/Firebase Authentication/app/src/main/java/com/jubayir/firebaseauthentication/LoginPageActivity.java
@@ -3,12 +3,31 @@ package com.jubayir.firebaseauthentication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPageActivity extends AppCompatActivity {
+
+    private EditText phoneNoEt;
+    private Button nextBtn;
+    private String phoneNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        phoneNoEt = findViewById(R.id.phoneEt);
+        nextBtn = findViewById(R.id.nextBtn);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phoneNo = phoneNoEt.getText().toString().trim();
+
+                if
+            }
+        });
     }
 }
