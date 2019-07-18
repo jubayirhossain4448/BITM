5f07b3a5c4a47966b2212709fac5a179b577eba7
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-19 00:25:32 +0600

    LoginPageActivity_Complete

--- Android_Home/Firebase Authentication/app/src/main/java/com/jubayir/firebaseauthentication/LoginPageActivity.java
+++ Android_Home/Firebase Authentication/app/src/main/java/com/jubayir/firebaseauthentication/LoginPageActivity.java
@@ -2,10 +2,12 @@ package com.jubayir.firebaseauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginPageActivity extends AppCompatActivity {
 
@@ -26,7 +28,14 @@ public class LoginPageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 phoneNo = phoneNoEt.getText().toString().trim();
 
-                if
+                if(phoneNo.length() == 11){
+                    Intent intent = new Intent(LoginPageActivity.this, VerifyActivity.class);
+                    intent.putExtra("Phone", phoneNo);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(LoginPageActivity.this, "Invalid Phone Number", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
