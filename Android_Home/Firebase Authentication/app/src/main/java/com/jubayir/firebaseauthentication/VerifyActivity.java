5a66e56219e9a95a7de367615ed93f286bf2d3da
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-19 22:51:11 +0600

    VerificationActivity_Complete

--- Android_Home/Firebase Authentication/app/src/main/java/com/jubayir/firebaseauthentication/VerifyActivity.java
+++ Android_Home/Firebase Authentication/app/src/main/java/com/jubayir/firebaseauthentication/VerifyActivity.java
@@ -1,10 +1,22 @@
 package com.jubayir.firebaseauthentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,11 +25,15 @@ public class VerifyActivity extends AppCompatActivity {
     private EditText verificationEt;
     private Button loginBtn;
     private String phoneNo;
+    private String verificationId;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         verificationEt = findViewById(R.id.verificationEt);
         loginBtn = findViewById(R.id.loginBtn);
@@ -25,6 +41,16 @@ public class VerifyActivity extends AppCompatActivity {
         phoneNo = getIntent().getStringExtra("Phone");
         
         sendOTP();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = verificationEt.getText().toString();
+                if (code.length() == 6){
+                    verify(code);
+                }
+            }
+        });
     }
 
     private void sendOTP() {
@@ -34,8 +60,45 @@ public class VerifyActivity extends AppCompatActivity {
                 60,                 // Timeout duration
                 TimeUnit.SECONDS,   // Unit of timeout
                 this,               // Activity (for callback binding)
-                mCallbacks);        // OnVerificationStateChangedCallbacksPhoneAuthActivity.java
+                callbacks);        // OnVerificationStateChangedCallbacksPhoneAuthActivity.java
     }
 
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks =
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null){
+                verificationEt.setText(code);
+                verify(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(VerifyActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+    };
+
+    private void verify(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Intent intent = new Intent(VerifyActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
