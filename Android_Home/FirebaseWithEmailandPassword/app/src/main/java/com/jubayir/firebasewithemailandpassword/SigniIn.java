c6f1a10cfcb211c6e72378b8e86a36611a8d7a00
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-20 23:48:47 +0600

    SignInActivity

---
+++ Android_Home/FirebaseWithEmailandPassword/app/src/main/java/com/jubayir/firebasewithemailandpassword/SigniIn.java
@@ -0,0 +1,14 @@
+package com.jubayir.firebasewithemailandpassword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigniIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signi_in);
+    }
+}
