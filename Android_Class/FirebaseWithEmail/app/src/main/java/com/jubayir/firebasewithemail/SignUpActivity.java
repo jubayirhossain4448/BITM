80e009f11119806eea80e54ad381eeff2ddfbca4
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2019-07-20 11:52:23 +0600

    firebaseWithEmail1

--- Android_Class/FirebaseWithEmail/app/src/main/java/com/jubayir/firebasewithemail/SignUpActivity.java
+++ Android_Class/FirebaseWithEmail/app/src/main/java/com/jubayir/firebasewithemail/SignUpActivity.java
@@ -2,13 +2,35 @@ package com.jubayir.firebasewithemail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button singIn, signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUp = findViewById(R.id.signUpSBtn);
+        singIn = findViewById(R.id.signInSBtn);
+
+        singIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+            }
+        });
     }
 }
