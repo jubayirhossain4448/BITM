1803711b8285c226b858c8bb20cd68fa02495fa6
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-21 23:50:34 +0600

    Databinding

--- Android_Home/FirebaseWithEmailandPassword/app/src/main/java/com/jubayir/firebasewithemailandpassword/SignUpActivity.java
+++ Android_Home/FirebaseWithEmailandPassword/app/src/main/java/com/jubayir/firebasewithemailandpassword/SignUpActivity.java
@@ -1,14 +1,23 @@
 package com.jubayir.firebasewithemailandpassword;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.jubayir.firebasewithemailandpassword.databinding.ActivitySignUpBinding;
+
+
 public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
     }
 }
