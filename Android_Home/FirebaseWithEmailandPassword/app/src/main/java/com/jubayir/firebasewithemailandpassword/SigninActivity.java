f3d9fdafb3de7c768dd0d1cd8e7b8c4485901076
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-22 00:40:11 +0600

    SignInActivity_Complete

--- Android_Home/FirebaseWithEmailandPassword/app/src/main/java/com/jubayir/firebasewithemailandpassword/SigninActivity.java
+++ Android_Home/FirebaseWithEmailandPassword/app/src/main/java/com/jubayir/firebasewithemailandpassword/SigninActivity.java
@@ -1,14 +1,28 @@
 package com.jubayir.firebasewithemailandpassword;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.jubayir.firebasewithemailandpassword.databinding.ActivitySignInBinding;
 
 public class SigninActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+
+        binding.signUpTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SigninActivity.this, SignUpActivity.class));
+            }
+        });
     }
 }
