6dceb18e680e781246ed3543d0bcc576be8da8d4
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-22 00:11:55 +0600

    SignUp

--- Android_Home/FirebaseWithEmailandPassword/app/src/main/java/com/jubayir/firebasewithemailandpassword/SignUpActivity.java
+++ Android_Home/FirebaseWithEmailandPassword/app/src/main/java/com/jubayir/firebasewithemailandpassword/SignUpActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.jubayir.firebasewithemailandpassword.databinding.ActivitySignUpBinding;
+
+import java.util.HashMap;
 
 
 public class SignUpActivity extends AppCompatActivity {
@@ -40,13 +47,30 @@ public class SignUpActivity extends AppCompatActivity {
                 String password = binding.passwordET.getText().toString().trim();
                 String cPassword = binding.confirmPasswordET.getText().toString().trim();
 
-                if (password.equals(cPassword)){
-                    
+                if (password.equals(cPassword)) {
+
                 }
             }
         });
     }
 
     private void init() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
+
+    private void SignUp(final String name, final String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+               String userId = firebaseAuth.getCurrentUser().getUid();
+               DatabaseReference userRef = databaseReference.child("users").child(userId);
+
+                    HashMap<String, Object> userMap = new HashMap<>();
+                }
+            }
+        });
+    }
+
 }
