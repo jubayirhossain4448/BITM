c3e05c664239d9e7c6d844a8b42a176768f3852e
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-23 19:50:03 +0600

    MainActivity

--- Android_Home/FirebaseCruid/app/src/main/java/com/jubayir/firebasecruid/MainActivity.java
+++ Android_Home/FirebaseCruid/app/src/main/java/com/jubayir/firebasecruid/MainActivity.java
@@ -1,14 +1,50 @@
 package com.jubayir.firebasecruid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private UserAdapter adapter;
+    private List<User> users;
+    private EditText nameET, ageET, emailET, bloodGroupET;
+    private Button addBtn;
+    private String name, age, email, bloodGroup;
+    
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
+    }
+
+    private void init() {
+
+        nameET = findViewById(R.id.nameET);
+        ageET = findViewById(R.id.ageET);
+        emailET = findViewById(R.id.emailET);
+        bloodGroupET = findViewById(R.id.bloodGroupET);
+
+        users = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.userRecyclearView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter(users);
+        recyclerView.setAdapter(adapter);
     }
 }
