b942ad433fbc4d10fa6511a5514cea9edbc481d8
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-23 13:01:25 +0600

    UserAdapter_complete

--- Android_Home/FirebaseCruid/app/src/main/java/com/jubayir/firebasecruid/UserAdapter.java
+++ Android_Home/FirebaseCruid/app/src/main/java/com/jubayir/firebasecruid/UserAdapter.java
@@ -1,4 +1,57 @@
 package com.jubayir.firebasecruid;
 
-public class UserAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private List<User> users;
+
+    public UserAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {  // layout Access korar jonno use hoii
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
+
+        User user = users.get(position);
+        holder.name.setText(user.getName());
+        holder.email.setText(user.getEmail());
+        holder.age.setText(user.getAge());
+        holder.bloodGroup.setText(user.getBloodGroup());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name, email, age, bloodGroup;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.nameTV);
+            email = itemView.findViewById(R.id.emailTV);
+            age = itemView.findViewById(R.id.ageTV);
+            bloodGroup = itemView.findViewById(R.id.bloodGroupTV);
+        }
+    }
 }
