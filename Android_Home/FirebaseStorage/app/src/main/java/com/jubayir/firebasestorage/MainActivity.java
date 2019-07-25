ecb1ebdfa1e992e5fe5724fdb679397af10919b0
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-25 23:16:07 +0600

    MainActivity

--- Android_Home/FirebaseStorage/app/src/main/java/com/jubayir/firebasestorage/MainActivity.java
+++ Android_Home/FirebaseStorage/app/src/main/java/com/jubayir/firebasestorage/MainActivity.java
@@ -1,11 +1,16 @@
 package com.jubayir.firebasestorage;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void selectImage(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
     }
 
     public void uploadImage(View view) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_OK && requestCode == 1 && data != null && data.getData() != null){
+
+            uri = data.getData();
+        }
     }
 }
