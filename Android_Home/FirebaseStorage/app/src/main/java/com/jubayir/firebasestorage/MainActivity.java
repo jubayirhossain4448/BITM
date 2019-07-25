f6985600e76d06db54823af95455c99411a313c3
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-25 23:09:04 +0600

    OnClick

--- Android_Home/FirebaseStorage/app/src/main/java/com/jubayir/firebasestorage/MainActivity.java
+++ Android_Home/FirebaseStorage/app/src/main/java/com/jubayir/firebasestorage/MainActivity.java
@@ -3,6 +3,7 @@ package com.jubayir.firebasestorage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectImage(View view) {
+    }
+
+    public void uploadImage(View view) {
     }
 }
