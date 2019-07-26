cf86a3a336ac99a0c4ca15c44b24b3fb9b76fa7a
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-26 23:48:15 +0600

    Main_Activity

--- Android_Home/GoogleMap/app/src/main/java/com/jubayir/googlemap/MainActivity.java
+++ Android_Home/GoogleMap/app/src/main/java/com/jubayir/googlemap/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync((OnMapReadyCallback) this);
     }
 }
