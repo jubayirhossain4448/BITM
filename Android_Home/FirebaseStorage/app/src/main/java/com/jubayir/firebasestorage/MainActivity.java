838a7949bf150da743c35471985e6520709ef20d
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-26 21:35:17 +0600

    Google_Map

--- Android_Home/FirebaseStorage/app/src/main/java/com/jubayir/firebasestorage/MainActivity.java
+++ Android_Home/FirebaseStorage/app/src/main/java/com/jubayir/firebasestorage/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
     }
 
     public void selectImage(View view) {
