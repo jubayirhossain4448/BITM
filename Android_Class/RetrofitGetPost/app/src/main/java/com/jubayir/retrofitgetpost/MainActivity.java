73b9034b374f664f6561342593d3ba201108eb86
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2019-07-27 11:52:59 +0600

    MainActivity

--- Android_Class/RetrofitGetPost/app/src/main/java/com/jubayir/retrofitgetpost/MainActivity.java
+++ Android_Class/RetrofitGetPost/app/src/main/java/com/jubayir/retrofitgetpost/MainActivity.java
@@ -1,15 +1,39 @@
 package com.jubayir.retrofitgetpost;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private List<User> users;
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        init();
+        
+        initRecyclearView();
+        
+    }
+
+    private void initRecyclearView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(userAdapter);
+    }
+
+    private void init() {
+        users = new ArrayList<>();
+        recyclerView = findViewById(R.id.userRecyclearView);
+
+        userAdapter = new UserAdapter(users);
     }
 }
