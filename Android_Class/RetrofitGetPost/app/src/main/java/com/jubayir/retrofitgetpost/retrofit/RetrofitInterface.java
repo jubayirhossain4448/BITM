dd027adc641ab3fa0eba497211dbe4b479d62854
BLACKCAT25896 <joykamrujjaman59@gmail.com>
2019-07-27 11:05:20 +0600

    RecyclearView

--- Android_Class/RetrofitGetPost/app/src/main/java/com/jubayir/retrofitgetpost/retrofit/RetrofitInterface.java
+++ Android_Class/RetrofitGetPost/app/src/main/java/com/jubayir/retrofitgetpost/retrofit/RetrofitInterface.java
@@ -1,4 +1,13 @@
 package com.jubayir.retrofitgetpost.retrofit;
 
+import com.jubayir.retrofitgetpost.User;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RetrofitInterface {
+
+    @GET("posts")
+    Call<User> getUser();
+
 }
