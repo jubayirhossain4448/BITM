6f764e87f3e8df32708060e4a7820fd665bb32bf
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-28 11:21:39 +0600

    UserAdapter_complete

--- Android_Home/Retrofit/app/src/main/java/com/jubayir/retrofit/UserAdapter.java
+++ Android_Home/Retrofit/app/src/main/java/com/jubayir/retrofit/UserAdapter.java
@@ -1,4 +1,56 @@
 package com.jubayir.retrofit;
 
-public class UserAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private List<User> users;
+
+    public UserAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.userId.setText(user.getUserId());
+        holder.id.setText(user.getId());
+        holder.title.setText(user.getTitle());
+        holder.body.setText(user.getBody());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView userId, id, title, body;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userId = itemView.findViewById(R.id.userIdTV);
+            id = itemView.findViewById(R.id.idTV);
+            title = itemView.findViewById(R.id.titleTV);
+            body = itemView.findViewById(R.id.bodyTV);
+        }
+    }
 }
