1a0518fe2a422bfb481ece4820b13b9ef33d17c8
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-28 11:02:51 +0600

    User_Call

--- Android_Home/Retrofit/app/src/main/java/com/jubayir/retrofit/retrofit/RetrofitInterface.java
+++ Android_Home/Retrofit/app/src/main/java/com/jubayir/retrofit/retrofit/RetrofitInterface.java
@@ -1,7 +1,12 @@
 package com.jubayir.retrofit.retrofit;
 
+import com.jubayir.retrofit.User;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface RetrofitInterface {
 
+    @GET("posts")
+    Call<User> getUser();
 }
