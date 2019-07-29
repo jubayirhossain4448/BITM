10db167a5059b96fda56c912bd110be0e5d7c518
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-30 00:18:28 +0600

    ApiClient_fields

--- Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/retrofit/ApiClient.java
+++ Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/retrofit/ApiClient.java
@@ -1,4 +1,9 @@
 package com.jubayir.retrofitcomments.retrofit;
 
+import retrofit2.Retrofit;
+
 public class ApiClient {
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    private static ApiClient mInstance;
+    private Retrofit retrofit;
 }
