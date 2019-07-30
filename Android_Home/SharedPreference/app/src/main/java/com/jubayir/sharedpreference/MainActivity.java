cf4fec4fb0ee23233b06080f68d538fd4499cec6
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-30 22:43:02 +0600

    MainActivity

--- Android_Home/SharedPreference/app/src/main/java/com/jubayir/sharedpreference/MainActivity.java
+++ Android_Home/SharedPreference/app/src/main/java/com/jubayir/sharedpreference/MainActivity.java
@@ -2,13 +2,38 @@ package com.jubayir.sharedpreference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText nameET, emailET;
+    String name, email;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameET = findViewById(R.id.nameET);
+        emailET = findViewById(R.id.emailET);
+    }
+
+    public void login(View view) {
+        name = nameET.getText().toString();
+        email = emailET.getText().toString();
+
+        preferences = getSharedPreferences("login", MODE_PRIVATE);
+        editor = preferences.edit();
+        editor.putString("name", name);
+        editor.putString("email", email);
+        editor.apply();
+
+        startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+
     }
 }
