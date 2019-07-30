a0c045e365e4e6612f86e40a16687ccfd29c2e51
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-30 21:57:37 +0600

    Access_Fields

--- Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/MainActivity.java
+++ Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/MainActivity.java
@@ -1,14 +1,30 @@
 package com.jubayir.retrofitcomments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.jubayir.retrofitcomments.retrofit.RetrofitInterface;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private UserAdapter adapter;
+    private List<User> users;
+    private RetrofitInterface retrofitInterface;
+    private Button insertBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
+    }
+
+    private void init() {
     }
 }
