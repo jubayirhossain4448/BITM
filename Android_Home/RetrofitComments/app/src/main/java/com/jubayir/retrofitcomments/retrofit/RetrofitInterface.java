80956ec09aaa7165a8e90930f25361c899ea2e68
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-30 12:14:46 +0600

    RetrofitInterface

--- Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/retrofit/RetrofitInterface.java
+++ Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/retrofit/RetrofitInterface.java
@@ -1,4 +1,11 @@
 package com.jubayir.retrofitcomments.retrofit;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RetrofitInterface {
+    @GET("comments")
+    Call<List<User>> getData();
 }
