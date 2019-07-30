718895b511054fabe79306c85d5a048883f809c0
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-07-30 22:06:06 +0600

    getData

--- Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/MainActivity.java
+++ Android_Home/RetrofitComments/app/src/main/java/com/jubayir/retrofitcomments/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.jubayir.retrofitcomments.retrofit.ApiClient;
 import com.jubayir.retrofitcomments.retrofit.RetrofitInterface;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -27,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         init();
 
         initRecyclearView();
+
+        getData();
+    }
+
+    private void getData() {
+      retrofitInterface = ApiClient.getInstance().getApi();
+
+        Call<List<User>> call = retrofitInterface.getData();
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                users = response.body();
+                adapter = new UserAdapter(users);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "" + t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     private void initRecyclearView() {
