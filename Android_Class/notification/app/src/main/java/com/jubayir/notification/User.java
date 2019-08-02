7e48cb33c79893265a2152c892a4ac1c0d2c3f0f
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-02 10:06:46 +0600

    UserClass

---
+++ Android_Class/notification/app/src/main/java/com/jubayir/notification/User.java
@@ -0,0 +1,28 @@
+package com.jubayir.notification;
+
+public class User {
+    private String email;
+    private String password;
+    private String name;
+
+    public User() {
+    }
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
