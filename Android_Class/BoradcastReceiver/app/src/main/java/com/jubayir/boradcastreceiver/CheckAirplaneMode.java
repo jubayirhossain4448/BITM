4889d134019ee385ca5ac75ed3c0b25e8fc33977
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-03 10:43:52 +0600

    MainActivity

--- Android_Class/BoradcastReceiver/app/src/main/java/com/jubayir/boradcastreceiver/CheckAirplaneMode.java
+++ Android_Class/BoradcastReceiver/app/src/main/java/com/jubayir/boradcastreceiver/CheckAirplaneMode.java
@@ -1,4 +1,26 @@
 package com.jubayir.boradcastreceiver;
 
-public class CheckAirplaneMode {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class CheckAirplaneMode extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() == Intent.ACTION_AIRPLANE_MODE_CHANGED) {
+            boolean isActive = isActive(context);
+
+            if (isActive) {
+                Toast.makeText(context, "Airplane mode on", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Airplane mode off", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean isActive(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, -1) != 0;
+    }
 }
