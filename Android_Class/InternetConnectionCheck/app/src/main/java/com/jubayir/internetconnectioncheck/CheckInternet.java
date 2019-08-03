15441d06b46877a258f14adbefb3243ef2ba9227
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-03 12:05:05 +0600

    MainActivity

--- Android_Class/InternetConnectionCheck/app/src/main/java/com/jubayir/internetconnectioncheck/CheckInternet.java
+++ Android_Class/InternetConnectionCheck/app/src/main/java/com/jubayir/internetconnectioncheck/CheckInternet.java
@@ -3,10 +3,26 @@ package com.jubayir.internetconnectioncheck;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
 
 public class CheckInternet extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        
+
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        if (isConnected){
+            Toast.makeText(context, "Internet is Connected", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Internet is not Connected", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
