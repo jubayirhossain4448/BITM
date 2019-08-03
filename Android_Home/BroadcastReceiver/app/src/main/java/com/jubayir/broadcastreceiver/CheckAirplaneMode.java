27ef7b3269ec768629ca6ac7c4f8a534c569eb72
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-04 00:29:54 +0600

    extends_BroadcastReceiver

--- Android_Home/BroadcastReceiver/app/src/main/java/com/jubayir/broadcastreceiver/CheckAirplaneMode.java
+++ Android_Home/BroadcastReceiver/app/src/main/java/com/jubayir/broadcastreceiver/CheckAirplaneMode.java
@@ -1,4 +1,12 @@
 package com.jubayir.broadcastreceiver;
 
-public class CheckAirplaneMode {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CheckAirplaneMode extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
 }
