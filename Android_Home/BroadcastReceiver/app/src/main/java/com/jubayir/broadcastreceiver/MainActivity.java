0ba5d6845129133eb0849f3bb042cd3fbed19d51
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-04 00:41:57 +0600

    MainActivity

--- Android_Home/BroadcastReceiver/app/src/main/java/com/jubayir/broadcastreceiver/MainActivity.java
+++ Android_Home/BroadcastReceiver/app/src/main/java/com/jubayir/broadcastreceiver/MainActivity.java
@@ -2,13 +2,30 @@ package com.jubayir.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import static android.content.Intent.ACTION_AIRPLANE_MODE_CHANGED;
+
 public class MainActivity extends AppCompatActivity {
+    private CheckAirplaneMode checkAirplaneMode = new CheckAirplaneMode();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(checkAirplaneMode, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(checkAirplaneMode);
     }
 }
