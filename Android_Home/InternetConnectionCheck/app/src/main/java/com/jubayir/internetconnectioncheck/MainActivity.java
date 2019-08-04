39853994e2970bf1de27f82af0d4f2cbe610fe36
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-05 00:01:00 +0600

    MainActivity

--- Android_Home/InternetConnectionCheck/app/src/main/java/com/jubayir/internetconnectioncheck/MainActivity.java
+++ Android_Home/InternetConnectionCheck/app/src/main/java/com/jubayir/internetconnectioncheck/MainActivity.java
@@ -2,13 +2,29 @@ package com.jubayir.internetconnectioncheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckInternet checkInternet = new CheckInternet();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(checkInternet, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(checkInternet);
     }
 }
