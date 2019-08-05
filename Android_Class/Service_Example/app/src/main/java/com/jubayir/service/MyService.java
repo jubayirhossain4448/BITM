b1381299d48dc721c27a7b15fe9b31fe5b4af607
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-05 10:44:26 +0600

    MyService

---
+++ Android_Class/Service_Example/app/src/main/java/com/jubayir/service/MyService.java
@@ -0,0 +1,12 @@
+package com.jubayir.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
