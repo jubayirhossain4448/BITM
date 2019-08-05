ea3d03aec6dccdf010ec13320221ac5e342927be
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-06 00:01:24 +0600

    Extends_Service

--- Android_Home/Service/app/src/main/java/com/jubayir/service/MyService.java
+++ Android_Home/Service/app/src/main/java/com/jubayir/service/MyService.java
@@ -1,4 +1,12 @@
 package com.jubayir.service;
 
-public class MyService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
