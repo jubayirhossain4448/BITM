4bde7a6fdaaa68a54b96267a203518d144287672
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-07 00:12:03 +0600

    TAG

--- Android_Home/Service/app/src/main/java/com/jubayir/service/MyService.java
+++ Android_Home/Service/app/src/main/java/com/jubayir/service/MyService.java
@@ -2,17 +2,23 @@ package com.jubayir.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.IBinder;
+import android.util.Log;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class MyService extends Service {
 
     @Override
     public void onCreate() {
+        Log.d(TAG, "onCreate: " + Thread.currentThread().getName());
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: " + Thread.currentThread().getName());
         return super.onStartCommand(intent, flags, startId);
     }
 
