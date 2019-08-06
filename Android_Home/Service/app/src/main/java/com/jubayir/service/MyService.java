1f8be6383b2d9b6f0af50f52efa629e451616cd7
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-08-07 00:05:48 +0600

    onDestroy

--- Android_Home/Service/app/src/main/java/com/jubayir/service/MyService.java
+++ Android_Home/Service/app/src/main/java/com/jubayir/service/MyService.java
@@ -15,9 +15,14 @@ public class MyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         return super.onStartCommand(intent, flags, startId);
     }
-    
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
